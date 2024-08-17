22ec48bdf5089a6c7e2f97145876329a2daf74ae
Anatoly Raev <cralixraev@gmail.com>
2024-08-17 16:27:23 +0300

    refactor(salon_lamp_types): use common vector for default color

--- lua/recipies/717_customization/salon_lamp_types.lua
+++ lua/recipies/717_customization/salon_lamp_types.lua
@@ -13,10 +13,11 @@ MEL.DefineRecipe("salon_lamp_types", "717_714")
 RECIPE.Description = "This recipe adds ability to customize salon lamp types."
 local MODELS_ROOT = "models/metrostroi_train/81-717/"
 local MAX_GLOW_COUNT = 48
+local COLOR_WHITE = Vector(255, 255, 255)
 local logError = MEL.LogErrorFactory()
 local function getLampData(wagon)
     local lamp_data = MEL.RecipeSpecific.SalonLampList[wagon:GetNW2Int("SalonLampType", 1)]
-    if string.find(wagon:GetClass(), "717") then
+    if MEL.Helpers.Is717(wagon:GetClass()) then
         return lamp_data.head
     else
         return lamp_data.int
@@ -195,11 +196,11 @@ function RECIPE:Inject(ent, entclass)
         end
 
         local rand = math.random() > 0.8 and 1 or math.random(0.95, 0.99)
-        local colors = {Vector(255, 255, 255)}
+        local colors = {COLOR_WHITE}
         if lamp_data.random_color then colors = lamp_data.random_color(lamp_amount) end
         local mean_color_sum, mean_count, mean_current_lamp_id = Vector(), 0, 11
         for i = 1, lamp_amount do
-            local color = colors[i] or Vector(255, 255, 255)
+            local color = colors[i] or COLOR_WHITE
             wagon:SetNW2Vector("SalonLampColor" .. i, color)
             wagon.SalonLamps.broken[i] = math.random() > rand and math.random() > 0.7
             if not lamp_data.light_color then
@@ -223,7 +224,6 @@ function RECIPE:Inject(ent, entclass)
             local light_color = lamp_data.light_color
             for i = 11, 13 do
                 wagon:SetNW2Vector("lampD" .. i, Vector(light_color.r, light_color.g, light_color.b))
-                -- wagon.Lights[i].brightness = lamp_data.light_brightness
             end
             return
         end
@@ -233,13 +233,12 @@ function RECIPE:Inject(ent, entclass)
             -- if we didn't changed colors on all lights, than just do it manually (but get a mean from ALL colors)
             mean_color_sum = Vector()
             for _, color in pairs(colors) do
-                mean_color_sum = mean_color_sum + (color or Vector(255, 255, 255))
+                mean_color_sum = mean_color_sum + (color or COLOR_WHITE)
             end
 
             local mean_color = (mean_color_sum / #colors) / 255
             for i = 11, 13 do
                 wagon:SetNW2Vector("lampD" .. i, Vector(mean_color.r, mean_color.g ^ 3, mean_color.b ^ 3) * 255)
-                -- wagon.Lights[i].brightness = lamp_data.light_brightness or 3
             end
         end
     end
