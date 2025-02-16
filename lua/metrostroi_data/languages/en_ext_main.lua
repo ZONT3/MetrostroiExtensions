0b8e9757e231c381cc685d4399ee524a83630093
Anatoly Raev <cralixraev@gmail.com>
2025-02-16 14:41:13 +0300

    fix: update languages to fix SeatType spawner translation

--- lua/metrostroi_data/languages/en_ext_main.lua
+++ lua/metrostroi_data/languages/en_ext_main.lua
@@ -9,8 +9,8 @@ Entities.gmod_subway_81-717_mvm_custom.Spawner.SalonLampType.Type1 = LPV-02
 Entities.gmod_subway_81-717_mvm_custom.Spawner.SalonLampType.Type2 = LLV-01
 
 # salon seats
-Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatType.Name = Salon seats
-Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatType.Random = @[Common.Spawner.Random]
-Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatType.Old = @[Common.Spawner.Old]
-Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatType.New = @[Common.Spawner.New]
+Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.Name = Salon seats
+Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.Random = @[Common.Spawner.Random]
+Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.Old = @[Common.Spawner.Old]
+Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.New = @[Common.Spawner.New]
 ]]
--- lua/metrostroi_data/languages/ru_ext_main.lua
+++ lua/metrostroi_data/languages/ru_ext_main.lua
@@ -2,14 +2,6 @@ return [[
 # Metrostroi Extensions russian translations
 
 [ru]
-# kv's
-Entities.gmod_subway_81-717_mvm_custom.Spawner.KvTypeCustom.Name = ГРКВ
-Entities.gmod_subway_81-717_mvm_custom.Spawner.KvTypeCustom.Random = @[Common.Spawner.Random]
-Entities.gmod_subway_81-717_mvm_custom.Spawner.KvTypeCustom.Black = Черный
-Entities.gmod_subway_81-717_mvm_custom.Spawner.KvTypeCustom.White = Белый
-Entities.gmod_subway_81-717_mvm_custom.Spawner.KvTypeCustom.Wood = Деревянный
-Entities.gmod_subway_81-717_mvm_custom.Spawner.KvTypeCustom.Yellow = Желтый
-
 # salon lamps
 Entities.gmod_subway_81-717_mvm_custom.Spawner.SalonLampType.Name = Лампы в салоне
 Entities.gmod_subway_81-717_mvm_custom.Spawner.SalonLampType.Random = @[Common.Spawner.Random]
@@ -17,8 +9,8 @@ Entities.gmod_subway_81-717_mvm_custom.Spawner.SalonLampType.Type1 = ЛПВ-02
 Entities.gmod_subway_81-717_mvm_custom.Spawner.SalonLampType.Type2 = ЛЛВ-01
 
 # salon seats
-Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatType.Name = Сидения в салоне
-Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatType.Random = @[Common.Spawner.Random]
-Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatType.Old = @[Common.Spawner.Old]
-Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatType.New = @[Common.Spawner.New]
+Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.Name = Сидения в салоне
+Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.Random = @[Common.Spawner.Random]
+Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.Old = @[Common.Spawner.Old]
+Entities.gmod_subway_81-717_mvm_custom.Spawner.SeatTypeCustom.New = @[Common.Spawner.New]
 ]]
